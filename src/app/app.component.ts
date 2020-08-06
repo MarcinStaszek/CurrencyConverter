0ac6cec1fea66e84e3f32ec5b6b1af1f8474969d
Marcin Staszek <marcin.staszek88@gmail.com>
2020-08-06 14:29:04 +0200

    add bootstrap, fix routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'currencyCalculator';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private router: Router) { }
+
+  routeHidden = true;
+
+  ngOnInit(): void {
+    this.router.events.subscribe( (e) => {
+      if (e instanceof NavigationStart) {
+        if (e.url === '/') {
+            this.routeHidden = true;
+        } else {
+            this.routeHidden = false;
+        }
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { ConverterComponent } from './converter/converter.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
-import { RouterModule, Routes, ROUTES } from '@angular/router';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
-const routes: Routes = [
-  { path: '', component: AppComponent },
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
 
 @NgModule({
   declarations: [
@@ -26,11 +15,13 @@ export class AppRoutingModule {}
     FooterComponent,
     ConverterComponent,
     HeaderComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
